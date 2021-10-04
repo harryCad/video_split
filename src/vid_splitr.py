f18ddebd8f4cc522edd3cc634436f17964c92e74
Harry Cadavid <hcadavid@pxmbp1.lan>
2021-10-04 00:32:39 -0700

    created a video writer from video file input

--- src/vid_splitr.py
+++ src/vid_splitr.py
@@ -5,30 +5,55 @@ https://learnopencv.com/reading-and-writing-videos-using-opencv/
 """
 import cv2
 
-vid_capture = cv2.VideoCapture('/Users/hcadavid/Documents/projects/video_split/temp_vid2.mov')
+vid_capture = cv2.VideoCapture('/Users/hcadavid/Documents/projects/video_split/input/temp_vid2.mov')
 if (vid_capture.isOpened() == False):
     print("Error opening the video file")
 else:
     print("Success")
     # available params instead of ints:
-    # https://www.geeksforgeeks.org/how-to-get-properties-of-python-cv2-videocapture-object/
+    # https://docs.opencv.org/4.5.2/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
     frame_persec = vid_capture.get(cv2.CAP_PROP_FPS) # .get(5)
     frame_count  = vid_capture.get(cv2.CAP_PROP_FRAME_COUNT) # .get(7)
-    # frame_height = vid_capture.get(cv2.CV_CAP_PROP_FRAME_HEIGHT)
-    # frame_width  = vid_capture.get(cv2.CV_CAP_PROP_FRAME_WIDTH)
+    frame_width  = int(vid_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(vid_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_size = (frame_width, frame_height)
 
+print('initiating video writer')
+#output = cv2.VideoWriter('Resources/output_video_from_file.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 20, frame_size)
+output = cv2.VideoWriter('/Users/hcadavid/Documents/projects/video_split/output/temp_frame.mp4'
+                        , cv2.VideoWriter_fourcc('X','V','I','D'), frame_persec, frame_size)
+
+print('opening and reading frames')
 while(vid_capture.isOpened()):
     # vid_capture.read() methods returns a tuple, first element is a bool
     # and the second is frame
     ret, frame = vid_capture.read()
     if ret == True:
-        cv2.imshow('Frame',frame)
+        print('reading frame:', frame)
+        #cv2.imshow('Frame',frame)
+        output.write(frame)
         # 20 is in milliseconds, try to increase the value, say 50 and observe
         key = cv2.waitKey(1000)
         if key == 113:
             break
     else:
         break
+
+# #output = cv2.VideoWriter('Resources/output_video_from_file.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 20, frame_size)
+# output = cv2.VideoWriter('/Users/hcadavid/Documents/projects/video_split/output/temp_frame.mp4'
+#                         , cv2.VideoWriter_fourcc(*'XVID'), frame_persec, frame_size)
+
+# while(vid_capture.isOpened()):
+#     # vid_capture.read() methods returns a tuple, first element is a bool
+#     # and the second is frame
+#     ret, frame = vid_capture.read()
+#     if ret == True:
+#         # Write the frame to the output files
+#         output.write(frame)
+#     else:
+#         print(‘Stream disconnected’)
+#         break
+
 
 vid_capture.release()
 cv2.destroyAllWindows()
